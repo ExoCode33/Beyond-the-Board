8abf1f0e9b2f4d97539b524e3b1c7229fe1f6256
ExoCode33 <emmanuel@supracode.ca>
2026-01-27 21:19:04 -0500

    Create explorer.js

---
+++ src/games/where-wind-meet/tier1/explorer.js
@@ -0,0 +1,14 @@
+export default {
+  id: 'wwm_t1_explorer',
+  name: 'Explorer',
+  description: 'Discover 10 different locations',
+  tier: 1,
+  game: 'where-wind-meet',
+  tokenReward: 15,
+  requiredImages: 2,
+  imageRequirements: [
+    'Map showing discovered locations',
+    'Statistics screen'
+  ],
+  verificationHints: 'Verify 10+ unique locations discovered.'
+};
