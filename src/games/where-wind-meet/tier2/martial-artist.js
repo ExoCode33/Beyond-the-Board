831726094a5972852d7995f66548caf5176c6f83
ExoCode33 <emmanuel@supracode.ca>
2026-01-27 21:19:57 -0500

    Create martial-artist.js

---
+++ src/games/where-wind-meet/tier2/martial-artist.js
@@ -0,0 +1,14 @@
+export default {
+  id: 'wwm_t2_martial_artist',
+  name: 'Martial Artist',
+  description: 'Master 5 combat techniques',
+  tier: 2,
+  game: 'where-wind-meet',
+  tokenReward: 25,
+  requiredImages: 2,
+  imageRequirements: [
+    'Skills menu showing 5 mastered techniques',
+    'Combat demonstration'
+  ],
+  verificationHints: 'Check skills are at max level.'
+};
